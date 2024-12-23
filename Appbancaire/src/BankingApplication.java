bc5b9cce7da63ad692a1f572cbf6a826658fc8c0
yassine jiddane <jiddaneyassine1@gmail.com>
2024-12-23 15:13:41 +0100

    add a validator class for regex

--- Appbancaire/src/BankingApplication.java
+++ Appbancaire/src/BankingApplication.java
@@ -1,4 +1,7 @@
 public class BankingApplication {
     public static void main(String[] args) {
+        System.out.println("bienvenu dans l'application bancaire !");
+        Bank bank = new Bank();
+        bank.run();
     }
 }
